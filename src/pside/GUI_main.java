aa966011049e06079aec3ab3d25b5712bdb987f5
Andrew Hild <republiqueajamais@hotmail.com>
2014-05-29 19:53:23 -0700

    The GUI is beginning to take form. Scaling buttons to size of icons and making buttons respond correctly is next

--- src/pside/GUI_main.java
+++ src/pside/GUI_main.java
@@ -1,25 +1,46 @@
 package pside;
 
 import cside.Board;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class GUI_main {
 	//Graphics objects
 	private JFrame frame;
 	private JPanel menu,data,field;
-	private JLabel mines, time;
-	private ArrayList<JLabel> tiles = new ArrayList<JLabel>();
+	//private JLabel mines, time;
+	private GridLayout minefield;
+	private ArrayList<JButton> tiles = new ArrayList<JButton>();
 	//Game objects
-	private Board board;
+	//private Board board;
 	public GUI_main(Board board) {
-		this.board=board;
+		//this.board=board;
 		frame=new JFrame("Minesweeper by Andrew Hild");
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		minefield=new GridLayout(board.getNumRows(),board.getNumCols());
+		field=new JPanel();
+		field.setLayout(minefield);
 		for(int q=0; q<(board.getNumRows()*board.getNumCols());q++)
 		{
-			JLabel j = new JLabel(new ImageIcon("src.pside.tile.png"));
+			JButton j = new JButton(new ImageIcon("src/pside/tile.png"));
 			tiles.add(j);
 		}
+		for(JButton button:tiles) {
+			field.add(button);
+		}
+		menu=new JPanel();
+		menu.add(new JLabel("Menu to go here"));
+		data=new JPanel();
+		data.add(new JLabel("Time elapsed and mines remaining data to go here"));
+		
+		
+		frame.getContentPane().add(menu, BorderLayout.PAGE_START);
+		frame.getContentPane().add(data, BorderLayout.CENTER);
+		frame.getContentPane().add(field, BorderLayout.PAGE_END);
 	}
 	
 	public void show() {
